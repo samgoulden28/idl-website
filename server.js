1283fe7f73e1ee65fdbce1f59c86739090d02a4e
sam goulden <samgoulden28@gmail.com>
2017-03-06 21:22:58 +0000

    Basic Mongo DB function

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var app = express();
 
+
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/test1');
+
 app.use(express.static('public'));
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
@@ -34,7 +44,11 @@ var data = {
 };
 
 app.get('/jade', function (req, res) {
-  res.render('layout', { test: JSON.stringify(data), standings:  JSON.stringify(currentStandings)} );
+  var db = req.db;
+  var collection = db.get('games');
+  collection.find({},{},function(e,docs){
+    res.render('layout', { test: JSON.stringify(data), standings:  currentStandings, games : JSON.stringify(docs)} );
+  });
 })
 
 var server = app.listen(8081, function () {
