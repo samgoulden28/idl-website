c0aa9a540ef6b0cf653a73ad3010eb395669e26f
samgoulden28 <sam-goulden@hotmail.co.uk>
2017-03-06 11:04:46 +0000

    JSON Support in jade

--- server.js
+++ server.js
@@ -8,11 +8,25 @@ app.set('view engine', 'jade')
 
 
 app.get('/', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.html");
+  res.sendFile( __dirname + "/" + "index.html");
 })
 
+var data = {
+    games: [
+    {
+      gameDate: '03/04/2017',
+      gameTitle: 'Team A vs Team B',
+      gameInfo: 'This is the info'
+    },
+    {
+      gameDate: '06/10/2017',
+      gameTitle: 'Team C vs Team D',
+      gameInfo: 'This is the info'
+    }]
+};
+
 app.get('/jade', function (req, res) {
-   res.render('layout');
+  res.render('layout', { test: JSON.stringify(data) } );
 })
 
 var server = app.listen(8081, function () {
